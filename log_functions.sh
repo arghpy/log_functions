8ea67897cc479a158900ff2b047dc4e337e4d79a
Arghpy <you@example.com>
2023-08-28 09:59:39 +0300

    added log_info

--- log_functions.sh
+++ log_functions.sh
@@ -32,9 +32,9 @@ log_it()
             log_color=${RED}
             log_status='ERROR'
             ;;
-        "PROGRESS")
+        "INFO")
             log_color=${LIGHT_CYAN}
-            log_status='PROGRESS'
+            log_status='INFO'
             ;;
     esac
 
@@ -47,7 +47,7 @@ log_it()
 log_ok() { LOG_LEVEL_FUNCTION="OK" ; log_it "$*"; }
 log_warning() { LOG_LEVEL_FUNCTION="WARNING" ; log_it "$*"; }
 log_error() { LOG_LEVEL_FUNCTION="ERROR" ; log_it "$*"; }
-log_progress() { LOG_LEVEL_FUNCTION="PROGRESS" ; log_it "$*"; }
+log_info() { LOG_LEVEL_FUNCTION="INFO" ; log_it "$*"; }
 
 
 log_check_function()
